fd3142e21c6ef1e500563e6629188a200d39acea
haoho98 <hao.ho@nfq.asia>
2020-04-05 08:25:37 +0700

    Do exercise communication in component

---
+++ Section_8_Component_Communication/excercise/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <app-header></app-header>
+    <hr />
+    <div class="row">
+      <app-list-server></app-list-server>
+      <app-sidebar></app-sidebar>
+    </div>
+    <hr />
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Shared/Header";
+import ListServer from "./components/Server/ListServer";
+import Footer from "./components/Shared/Footer";
+import Sidebar from "./components/Shared/Sidebar";
+export default {
+  components: {
+    'app-header': Header,
+    'app-list-server': ListServer,
+    'app-sidebar': Sidebar,
+    'app-footer': Footer
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ Section_8_Component_Communication/excercise/src/components/Shared/Sidebar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="col-xs-12 col-sm-6">
+    <p>Server Detail</p>
+    <p>Server# {{this.serverDetail.id}} - Status: {{this.serverDetail.status}}</p>
+    <button @click="changeStatus">Change to Normal</button>
+  </div>
+</template>
+
+<script>
+import { eventBus } from "../../main";
+export default {
+  data: function() {
+    return {
+      serverDetail: {}
+    };
+  },
+  methods: {
+    changeStatus(){
+      this.serverDetail.status = 'Normal';
+    }
+  },
+  created() {
+    eventBus.$on("emitServer", detail => {
+      this.serverDetail = detail;
+    });
+  }
+};
+</script>
+
+<style scoped>
+div {
+  border: 1px solid blue;
+}
+</style>
