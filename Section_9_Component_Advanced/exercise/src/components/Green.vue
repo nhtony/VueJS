cacad2db5641823eb726d11a7be437da53e19429
haoho98 <hao.ho@nfq.asia>
2020-04-05 09:32:02 +0700

    Do excercise Advanced Component

---
+++ Section_9_Component_Advanced/exercise/src/components/Green.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+         <h2>This is a Green box</h2>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    div {
+        border: 1px solid green;
+        background-color: lightgreen;
+        padding: 30px;
+        margin: 20px auto;
+        text-align: center
+    }
+</style>
