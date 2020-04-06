6057d1205a9725dca9d6ac3d01352f061fe83688
haoho98 <hao.ho@nfq.asia>
2020-04-06 08:20:32 +0700

    recommit

--- Section_10_Wonderful_Quotes/project/src/components/NewQuote.vue
+++ Section_10_Wonderful_Quotes/project/src/components/NewQuote.vue
@@ -3,7 +3,7 @@
     <div class="col-sm-8 col-sm-offset-2 col-md-4 col-xs-12 col-md-6 col-md-offset-3 form-group">
         <label>Quote</label>
         <textarea class="form-control" row="3" v-model="quote"></textarea>
-        <button class="btn btn-primary" @click.prevent="createNew">Add Quote</button>
+        <button class="btn btn-primary" @click.prevent="createNew">Add New Quote</button>
     </div>
   </div>
 </template>
