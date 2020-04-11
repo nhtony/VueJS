ebe79b83b0ac3ae70be0df5fa7ba0e60359ad101
haoho98 <hao.ho@nfq.asia>
2020-04-11 14:21:48 +0700

    Learned Routing

---
+++ Section_16_Routing/learn/src/components/user/User.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>The User Page</h1>
+    <hr />
+    <button @click="navigateToHome">Go to Home</button>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      id: this.$route.params.id
+    };
+  },
+  // Follow the change of id
+  watch: {
+    $route(to, from) {
+      this.id = to.params.id;
+    }
+  },
+  methods: {
+    navigateToHome() {
+      this.$router.push({ name: 'home' });
+    }
+  }
+};
+</script>
---
+++ Section_16_Routing/learn/src/components/user/UserDetail.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h3>Some User Details</h3>
+    <p>User loaded ID: {{id}}</p>
+    <router-link tag="button" :to="link" class="btn btn-primary">Edit User</router-link>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      id: this.$route.params.id,
+      link: {
+        name: "editUser",
+        params: { id: this.$route.params.id },
+        query: { locale: "en", q: 100 },
+        hash: "#data"
+      }
+    };
+  },
+  beforeRouteEnter(to, from, next) {
+    // can not use this.link here
+    if (true) {
+      next();
+    } else{
+      next(false);
+    }
+  }
+};
+</script>
